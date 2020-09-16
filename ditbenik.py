5b1b2b016dcddbd9f5f474273ea0110ef56a7418
KilianB95 <70953273+KilianB95@users.noreply.github.com>
2020-09-16 11:50:03 +0200

    dit ben ik opdracht

---
+++ ditbenik.py
@@ -0,0 +1,35 @@
+print("Dit is mijn HelloYou textbased applicatie")
+print("ik ben Kilian\n")
+jouwNaam = input("hoee heet jij? ")
+print (jouwNaam + " welkom in mijn textbased applicatie")
+print("weet jij naar wat voor soort muziek ik het liefst luister?")
+print("a rock \nb metal \nc hiphop \nd blues rock")
+antwoord1 = input("a b c of d ")
+print("jou antwoord is " + antwoord1)
+ 
+ 
+if antwoord1 == "a" or antwoord1 == "A":
+      print("vindt ik ook leuk alleen luister ik niet het liefst")
+if antwoord1 == "b" or antwoord1 == "B":
+      print("dat is juist, ik luister graag metal")
+if antwoord1 == "c" or antwoord1 == "C":
+      print("ugghh nee echt totaal nee afschuwelijke muziek")
+if antwoord1 == "d" or antwoord1 == "D":
+      print("Blues Rock is op zijn tijd leuk, maar niet wat ik vaak luister")
+      
+
+
+
+print("nu je mijn muziek style weet, kan je dan ook raden hoe oud ik ben")
+print("a 19 \nb 23 \nc 25 \nd ??")
+antwoord2 = input("a b c of d ")
+print("jou antwoord is " + antwoord2)
+
+
+if antwoord2 == "a" or antwoord2 == "A":
+    print("was ik maar weer deze leeftijd")
+if antwoord2 == "b" or antwoord2 == "B":
+    print("je zit dichtbij, ik ben 25")
+if antwoord2 == "c" or antwoord2 == "C":
+    print("dat is inderdaad juist ik ben 25")
+
