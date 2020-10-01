2c167ac089d3f491227affdd72e793e608a5b858
Kilian <31303@ma-web.nl>
2020-10-01 11:16:56 +0200

    3de vraag erbij toegevoegd

--- ditbenik.py
+++ ditbenik.py
@@ -21,7 +21,7 @@ if antwoord1 == "d" or antwoord1 == "D":
 
 
 print("nu je mijn muziek style weet, kan je dan ook raden hoe oud ik ben")
-print("a 19 \nb 23 \nc 25 \nd ??")
+print("a 19 \nb 23 \nc 25 \nd 30")
 antwoord2 = input("a b c of d ")
 print("jou antwoord is " + antwoord2)
 
@@ -31,5 +31,24 @@ if antwoord2 == "a" or antwoord2 == "A":
 if antwoord2 == "b" or antwoord2 == "B":
     print("je zit dichtbij, ik ben 25")
 if antwoord2 == "c" or antwoord2 == "C":
-    print("dat is inderdaad juist ik ben 25") 
+    print("dat is inderdaad juist ik ben 25")
+if antwoord2 == "d" or antwoord2 == "D":
+      print("Hold up ik ben nog niet zo oud ben maar 25 tot nu toe oud zat haha")
 
+print("Nu weet je hoe oud ik ben, maar hoe denk je dat ik in elkaar zit?")
+print("a rustig \nb druk \nc stil \nd gestoord")
+antwoord3 = input("a b c of d")
+print("jou antwoord is " + antwoord3)
+
+
+if antwoord3 == "a" or antwoord3 == "A":
+      print("Klopt inderdaad ik ben rustig")
+if antwoord3 == "b" or antwoord3 == "B":
+      print("Ik kan druk zijn maar dat is niet hoe ik echt in elkaar zit ben erg rustig van mezelf")
+if antwoord3 == "c" or antwoord3 == "C":
+      print("Ik ben niet stil ik hou wel van een conversatie maar rustig was het juiste antwoord")
+if antwoord3 == "d" or antwoord3 == "D":
+      print("Gestoord ArE YoU KiDdInG mE ?????? Ik BeN RuStIg HoOr Ik BeN nIeT gEsToOr ;p")
+
+print("Bedankt voor het mee doen aan mijn 3 vragen en dat je een beetje weet wie ik ben. zoals je weet hou ik van metal, ben ik 25 en ben ik een rustig persoon.")
+print("Fijne dag verder!!")
